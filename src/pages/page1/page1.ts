da8916613a8bd022d61be782826173f3977853d1
Suvojit Kar <suvojitkar@arubanetworks.com>
2017-03-21 19:30:43 +0530

    actionsheet added

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { LoadingController } from 'ionic-angular';
+import { Platform, ActionSheetController } from 'ionic-angular';
 
 
 @Component({
@@ -10,8 +11,48 @@ import { LoadingController } from 'ionic-angular';
 })
 export class Page1 {
 
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController,  public platform: Platform,
+    public actionsheetCtrl: ActionSheetController, public loadingCtrl: LoadingController) {
      this.presentLoading();
+  }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Video',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
   
   presentLoading() {
